03e30fe1ce52bd700b8b346159f1442b7967f880
mthiagoalves <m.thiago.alves@live.com>
2022-04-13 23:07:28 -0300

    andamento projeto

---
+++ assets/script.js
@@ -0,0 +1,58 @@
+const baseURL = "http://localhost:3000/guitars";
+
+async function findAllGuitars() {
+  const response = await fetch(`${baseURL}/find-guitar`);
+
+  const guitars = await response.json();
+
+  guitars.forEach(function (guitars) {
+    document.querySelector("#container").insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="card">
+        <div class="img-card">
+          <img src="${guitars.img}" />
+        </div>
+        <div class="content">
+          <h2>${guitars.name}</h2>
+          <p class="model">${guitars.model}</p>
+          <p class="description">
+          ${guitars.description}
+          </p>
+          <span class="price">$ ${guitars.price}</span>
+        </div>
+      </div>
+    `
+    );
+  });
+}
+
+async function findByIdGuitars() {
+  const id = document.querySelector("#id-guitar").value;
+
+  const response = await fetch(`${baseURL}/guitar/${id}`);
+
+  const guitar = await response.json();
+
+  console.log(guitar);
+
+  const guitarSelect = document.querySelector(".selected-guitar");
+
+  guitarSelect.innerHTML = `
+    <div class="card-item">
+      <div class="img-card-item">
+        <img src="${guitar.img}"/>
+      </div>
+      <div class="content-item">
+        <h2>${guitar.name}</h2>
+        <p class="model">${guitar.model}</p>
+        <p class="description">
+        ${guitar.description}
+        </p>
+        <span class="price">$ ${guitar.price}</span>
+      </div>
+    </div>
+  `;
+}
+
+findAllGuitars();
